6f8d660b9c15a75b6ddeea26f80bfaa48b4b1166
jamie <jamie.coales@education.gov.uk>
2018-10-10 09:39:50 +0100

    #60949 add partial class and interface around rulebase dbcontext to allow external use by ESF

---
+++ src/ESFA.DC.ILR1819.DataStore.EF/ILR1819_DataStoreEntities.cs
@@ -0,0 +1,13 @@
+﻿using ESFA.DC.ILR1819.DataStore.EF.Interfaces;
+
+namespace ESFA.DC.ILR1819.DataStore.EF
+{
+    public partial class ILR1819_DataStoreEntities : IILR1819_DataStoreEntities
+    {
+        public ILR1819_DataStoreEntities(string connectionString)
+            : base(connectionString)
+        {
+            
+        }
+    }
+}
