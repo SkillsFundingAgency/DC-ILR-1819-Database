974e6cf871841e8fb6ed9b6fd28bd901ab4778b5
jamie <jamie.coales@education.gov.uk>
2018-10-17 15:40:25 +0100

    #61144 Add partial class and interface to valid schema for external access

---
+++ src/ESFA.DC.ILR1819.DataStore.EF.Valid/ILR1819_DataStoreEntitiesValid.cs
@@ -0,0 +1,13 @@
+﻿using ESFA.DC.ILR1819.DataStore.EF.Valid.Interfaces;
+
+namespace ESFA.DC.ILR1819.DataStore.EF.Valid
+{
+    public partial class ILR1819_DataStoreEntitiesValid : IILR1819_DataStoreEntitiesValid
+    {
+        public ILR1819_DataStoreEntitiesValid(string connectionString)
+            : base(connectionString)
+        {
+            
+        }
+    }
+}
