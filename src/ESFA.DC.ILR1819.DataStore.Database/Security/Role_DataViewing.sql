a7931d6a589a0dd3cc5d91df5868dd06384926ef
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-11-07 12:04:47 +0000

    DB : BugFix : Add Missing Schema to DataViewing Role as only dbo was allowed.

--- src/ESFA.DC.ILR1819.DataStore.Database/Security/Role_DataViewing.sql
+++ src/ESFA.DC.ILR1819.DataStore.Database/Security/Role_DataViewing.sql
@@ -8,6 +8,41 @@ GRANT
 	SELECT, 
 	VIEW DEFINITION 
 ON SCHEMA::dbo
-	TO [DataViewing]
+	TO DataProcessing
 GO
 
+-- Grant access rights to a specific schema in the database
+GRANT 
+REFERENCES, 
+	SELECT, 
+	VIEW DEFINITION 
+ON SCHEMA::Invalid
+	TO DataProcessing
+GO
+
+-- Grant access rights to a specific schema in the database
+GRANT 
+	REFERENCES, 
+	SELECT, 
+	VIEW DEFINITION 
+ON SCHEMA::Valid
+	TO DataProcessing
+GO
+
+-- Grant access rights to a specific schema in the database
+GRANT 
+	REFERENCES, 
+	SELECT, 
+	VIEW DEFINITION 
+ON SCHEMA::Rulebase
+	TO DataProcessing
+GO
+
+-- Grant access rights to a specific schema in the database
+GRANT 
+	REFERENCES, 
+	SELECT, 
+	VIEW DEFINITION 
+ON SCHEMA::DataLock
+	TO DataProcessing
+GO
