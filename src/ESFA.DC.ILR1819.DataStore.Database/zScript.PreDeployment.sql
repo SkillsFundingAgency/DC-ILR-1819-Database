e5f1f66f3b97864f765b25c8612fd1c88096de23
Peter Chamley <13586812+PJChamley@users.noreply.github.com>
2018-11-07 12:07:39 +0000

    DB : Remove code from Pre-DeploymentScript as Database updates have been deployed.

--- src/ESFA.DC.ILR1819.DataStore.Database/zScript.PreDeployment.sql
+++ src/ESFA.DC.ILR1819.DataStore.Database/zScript.PreDeployment.sql
@@ -1,41 +1,6 @@
 ﻿/*
- Pre-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be executed before the build script.	
- Use SQLCMD syntax to include a file in the pre-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the pre-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
+ Pre-Deployment Script 
 --------------------------------------------------------------------------------------
 */
 SET NOCOUNT ON;
 GO
-
---RAISERROR('		   Update valid.LearningDeliveryFAM.LearningDeliveryFAM_Id',10,1) WITH NOWAIT;
---GO
---DROP SEQUENCE IF EXISTS  [Valid].[LearningDeliveryFAM_SEQUENCE]
---GO
---CREATE SEQUENCE [Valid].[LearningDeliveryFAM_SEQUENCE]
---    START WITH 1  
---    INCREMENT BY 1 ;  
---GO
---RAISERROR('		   CREATE SEQUENCE',10,1) WITH NOWAIT;
---GO
---IF EXISTS
---(SELECT * FROM INFORMATION_SCHEMA.COLUMNS C
---WHERE C.TABLE_SCHEMA = 'Valid' 
---  AND C.TABLE_NAME = 'LearningDeliveryFAM'
---  AND C.COLUMN_NAME = 'LearningDeliveryFAM_Id'
---) 
---BEGIN
---	RAISERROR('		   Start Update.',10,1) WITH NOWAIT;
---	EXEC ('UPDATE O SET [LearningDeliveryFAM_Id] = NEXT VALUE FOR [Valid].[LearningDeliveryFAM_SEQUENCE] FROM [Valid].[LearningDeliveryFAM] O')
---	RAISERROR('		     End Update.',10,1) WITH NOWAIT;
-	
---END
---GO
---DROP SEQUENCE IF EXISTS  [Valid].[LearningDeliveryFAM_SEQUENCE]
---GO
---RAISERROR('		   Update valid.LearningDeliveryFAM.LearningDeliveryFAM_Id END',10,1) WITH NOWAIT;
---GO
--- src/ESFA.DC.ILR1819.DataStore.Database/zzScript.PostDeployment.sql
+++ src/ESFA.DC.ILR1819.DataStore.Database/zzScript.PostDeployment.sql
@@ -1,15 +1,7 @@
 ﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
+Post-Deployment Script 
 --------------------------------------------------------------------------------------
 */
-
 
 
 RAISERROR('		   Extended Property',10,1) WITH NOWAIT;
